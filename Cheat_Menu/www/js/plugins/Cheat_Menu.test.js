2f494313f16e2299d784af0d9168aa4526b01393
Nicholas Wright <dozedoffagain@gmail.com>
2024-12-25 13:42:26 +0100

    Added Jest unit testing

---
+++ Cheat_Menu/www/js/plugins/Cheat_Menu.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/**
+ * Mock to get tests running
+ */
+DataManager = {};
+
+const sut = require('./Cheat_Menu');
+
+
+test('Dummy test', () => {
+    expect(42).toBe(42);
+});
